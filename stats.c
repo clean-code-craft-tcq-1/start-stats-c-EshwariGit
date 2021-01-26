e552aafe1f544d51b8103d4940554ff4dce6bf4c
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-01-27 00:05:06 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -57,6 +57,16 @@ s.min = usenum[0];
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
+void emailAlerter(void)
+{
+    emailAlertCallCount++;
+}
+
+void ledAlerter(void)
+{
+    ledAlertCallCount++;
+}
+
 void check_and_alert(const float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
     if(computedStats.max>maxThreshold)
